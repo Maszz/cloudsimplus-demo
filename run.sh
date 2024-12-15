8c9f70d6595e61d39623f97c5000c60290287d2e
Supakrit Nithikethkul <6410615139@student.tu.ac.th>
2024-12-15 15:34:33 +0700

    Initial energy simulate

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Step 0: generate new pom.xml
+# Replace placeholders with values
+template="pom.xml.template"
+output="pom.xml"
+sed -e "s|\${project}|$1|g" \
+    "$template" > "$output"
+echo "Generated $output from $template."
+
 # Step 1: Clean, copy dependencies, and build the project
 echo "Running Maven build..."
 mvn clean dependency:copy-dependencies package
@@ -12,7 +20,7 @@ fi
 
 # Step 3: Run the Java application
 echo "Running the application..."
-java -cp "target/demo-1.0-SNAPSHOT.jar:target/dependency/*" com.example.cloudsim.demo.Main
+java -cp "target/$1-1.0-SNAPSHOT.jar:target/dependency/*" "com.example.cloudsim.$1.Main"
 
 # Step 4: Check if the application ran successfully
 if [ $? -ne 0 ]; then
