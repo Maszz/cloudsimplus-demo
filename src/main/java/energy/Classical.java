019d6b39198a1e9edb0560abd926b784a90b6c83
Supakrit Nithikethkul <6410615139@student.tu.ac.th>
2025-02-10 13:23:25 +0700

    Fix: Classical muliple datacenter simulation

--- src/main/java/energy/Classical.java
+++ src/main/java/energy/Classical.java
@@ -1,7 +1,5 @@
 package energy;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import org.cloudsimplus.allocationpolicies.VmAllocationPolicyBestFit;
 import org.cloudsimplus.brokers.DatacenterBrokerSimple;
 import org.cloudsimplus.builders.tables.CloudletsTableBuilder;
@@ -17,96 +15,113 @@ import org.cloudsimplus.resources.Pe;
 import org.cloudsimplus.resources.PeSimple;
 import org.cloudsimplus.vms.Vm;
 import org.cloudsimplus.vms.VmSimple;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Classical {
-    private int ramPerPe;
-    private int hosts;
-    private int hostPes;
-    private int hostMips;
-    private long hostBw;
-    private long hostStorage;
-    private int vms;
-    private int vmPes;
-    private int cloudletLength;
-    private double schedulingInterval;
+    private static Config config;
+    private CloudSimPlus simulation;
+    private DatacenterBrokerSimple broker;
+    private List<Datacenter> datacenters;
+    private List<Vm> vmList;
+    private List<Cloudlet> cloudletList;
 
     public static void main(String[] args) {
         if (args.length < 1) {
-            System.err.println("Usage: java Classical <config-file>");
+            System.err.println("Usage: java Classical <config.json>");
             System.exit(1);
         }
-
-        Config config = new Config(args[0]);
-        new Classical(config).run();
-    }
-
-    public Classical(Config config) {
-        this.ramPerPe = config.getInt("RAM_PER_PE");
-        this.hosts = config.getInt("HOSTS");
-        this.hostPes = config.getInt("HOST_PES");
-        this.hostMips = config.getInt("HOST_MIPS");
-        this.hostBw = config.getLong("HOST_BW");
-        this.hostStorage = config.getLong("HOST_STORAGE");
-        this.vms = config.getInt("VMS");
-        this.vmPes = config.getInt("VM_PES");
-        this.cloudletLength = config.getInt("CLOUDLET_LENGTH");
-        this.schedulingInterval = Math.max(1.0, config.getDouble("SCHEDULING_INTERVAL"));
+        config = new Config(args[0]); // ✅ Uses Config.java to read JSON
+        new Classical().run();
     }
 
     public void run() {
-        CloudSimPlus simulation = new CloudSimPlus();
+        simulation = new CloudSimPlus();
+        broker = new DatacenterBrokerSimple(simulation);
 
-        List<Host> hostList = createHosts();
-        Datacenter datacenter = new DatacenterSimple(simulation, hostList, new VmAllocationPolicyBestFit());
-        datacenter.setSchedulingInterval(schedulingInterval);
+        datacenters = createDatacenters();
+        vmList = createVms();
+        cloudletList = createCloudlets();
 
-        DatacenterBrokerSimple broker = new DatacenterBrokerSimple(simulation);
+        broker.submitVmList(vmList);
+        broker.submitCloudletList(cloudletList);
 
-
-        broker.submitVmList(createVms());
-        broker.submitCloudletList(createCloudlets());
-
+        System.out.println("Starting simulation...");
         simulation.start();
 
         new CloudletsTableBuilder(broker.getCloudletFinishedList()).build();
     }
 
-    private List<Host> createHosts() {
-        List<Host> hostList = new ArrayList<>(hosts);
-        for (int i = 0; i < hosts; i++) {
-            List<Pe> peList = new ArrayList<>(hostPes);
+    private List<Datacenter> createDatacenters() {
+        List<Datacenter> datacenterList = new ArrayList<>();
+        JsonArray datacentersConfig = config.getArray("DATACENTERS");
+
+        for (JsonElement element : datacentersConfig) {
+            JsonObject dcConfig = element.getAsJsonObject(); // ✅ Convert JsonElement to JsonObject
+            datacenterList.add(createDatacenter(dcConfig));
+        }
+
+        return datacenterList;
+    }
+
+    private Datacenter createDatacenter(JsonObject dcConfig) {
+        String name = dcConfig.get("name").getAsString();
+        int numHosts = dcConfig.get("hosts").getAsInt();
+        int hostPes = dcConfig.get("HOST_PES").getAsInt();
+        int hostMips = dcConfig.get("HOST_MIPS").getAsInt();
+        int hostRam = dcConfig.get("HOST_RAM").getAsInt();
+        int hostBw = dcConfig.get("HOST_BW").getAsInt();
+        long hostStorage = dcConfig.get("HOST_STORAGE").getAsLong();
+
+        List<Host> hostList = new ArrayList<>();
+        for (int i = 0; i < numHosts; i++) {
+            List<Pe> peList = new ArrayList<>();
             for (int j = 0; j < hostPes; j++) {
                 peList.add(new PeSimple(hostMips / hostPes));
             }
-            Host host = new HostSimple(ramPerPe * hostPes, hostBw, hostStorage, peList);
-            host.setPowerModel(new PowerModelHostSimple(800, 80));
+            Host host = new HostSimple(hostRam, hostBw, hostStorage, peList);
+            host.setPowerModel(new PowerModelHostSimple(800, 80)); // Power consumption model
             hostList.add(host);
         }
-        return hostList;
+        System.out.printf("✅ Created Datacenter: %s with %d hosts%n", name, numHosts);
+        return new DatacenterSimple(simulation, hostList, new VmAllocationPolicyBestFit());
     }
 
     private List<Vm> createVms() {
-        List<Vm> vmList = new ArrayList<>(vms);
-        for (int i = 0; i < vms; i++) {
-            Vm vm = new VmSimple(hostMips, vmPes)
-                    .setRam(vmPes * ramPerPe)
-                    .setBw(hostBw / vmPes)
-                    .setSize(10000);
+        int numVms = config.getInt("VMS");
+        int vmPes = config.getInt("VM_PES");
+        int vmRam = config.getInt("VM_RAM");
+        int vmBw = config.getInt("VM_BW");
+        int vmStorage = config.getInt("VM_STORAGE");
+
+        List<Vm> vmList = new ArrayList<>();
+        for (int i = 0; i < numVms; i++) {
+            Vm vm = new VmSimple(vmPes * 1000, vmPes)
+                    .setRam(vmRam)
+                    .setBw(vmBw)
+                    .setSize(vmStorage);
             vmList.add(vm);
         }
+        System.out.printf("✅ Created %d VMs%n", numVms);
         return vmList;
     }
 
     private List<Cloudlet> createCloudlets() {
-        List<Cloudlet> cloudletList = new ArrayList<>(vms);
-        for (int i = 0; i < vms; i++) {
-            Cloudlet cloudlet = new CloudletSimple(cloudletLength, vmPes);
+        int numCloudlets = config.getInt("CLOUDLETS");
+        int cloudletPes = config.getInt("CLOUDLET_PES");
+        int cloudletLength = config.getInt("CLOUDLET_LENGTH");
+
+        List<Cloudlet> cloudletList = new ArrayList<>();
+        for (int i = 0; i < numCloudlets; i++) {
+            Cloudlet cloudlet = new CloudletSimple(cloudletLength, cloudletPes);
             cloudlet.setSizes(1024);
             cloudletList.add(cloudlet);
         }
+        System.out.printf("✅ Created %d Cloudlets%n", numCloudlets);
         return cloudletList;
     }
 }
